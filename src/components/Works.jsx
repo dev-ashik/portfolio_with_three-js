8566789b744a88fe1c75065308c5eed27618532d
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-03-13 10:35:47 +0800

    work page text effect done done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -13,22 +13,56 @@ const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
   display: flex;
-  justify-content; center;
+  justify-content: center;
 `
 const Container = styled.div`
-  width: 4400px;
+  width: 1400px;
   display: flex;
   justify-content: space-between;
 `
 const Left = styled.div`
   flex: 1;
   display: flex;
+  align-items: center;
 `
 const List = styled.ul`
-  
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `
 const ListItem = styled.li`
-  
+  font-size: 90px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+
+
+  ::after {
+    content: "${(props) => props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    color: pink;
+    width: 0;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  &:hover {
+    ::after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to {
+          width: 100%;
+        }
+      }
+    }
+  }
 `
 
 const Right = styled.div`
@@ -43,7 +77,7 @@ export const Works = () => {
           <List>
             {
               data.map((item) => (
-                <ListItem key={item}>{item}</ListItem>
+                <ListItem key={item} text={item}>{item}</ListItem>
               ))
             }
           </List>
